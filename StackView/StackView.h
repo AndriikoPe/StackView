f607148bee4c2997e157314d39dd4d6d53980aad
Andriiko <andrii.permiakov@getimpulse.app>
2022-12-11 13:04:44 +0200

    Initial commit

---
+++ StackView/StackView.h
@@ -0,0 +1,18 @@
+//
+//  StackView.h
+//  StackView
+//
+//  Created by Andriiko on 11.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StackView.
+FOUNDATION_EXPORT double StackViewVersionNumber;
+
+//! Project version string for StackView.
+FOUNDATION_EXPORT const unsigned char StackViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StackView/PublicHeader.h>
+
+
